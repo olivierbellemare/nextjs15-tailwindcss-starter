1b7de7e556cab26bb24bb289d310b5d91a4ec71f
Olivier Bellemare <o.bellemare@soinsamika.com>
2025-09-22 11:33:35 -0400

    add component card

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="bg-plum p-4 gap-4 flex">
+    <nav className="bg-White30 backdrop-blur-xs p-4 gap-4 flex">
       <Link href="./" className={`text-White70 hover:text-gray-300 ${ pathname === '/' ? 'underline' : '' }`}>Home</Link>
       <Link href="/about" className={`text-White70 hover:text-gray-300 ${ pathname === '/about' ? 'underline' : '' }`}>About</Link>
       <Link href="/contact" className={`text-White70 hover:text-gray-300 ${ pathname === '/contact' ? 'underline' : '' }`}>Contact</Link>   
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import NextLogo from "@components/NextLogo";
-import VercelLogo from "@components/VercelLogo";
+import Link from "@node_modules/next/link";
 export default function Home() {
   return (
     <>
-      <section className="container mx-auto text-center bg-red-400">
+      <section className="container mx-auto text-center">
         <h1>Home page</h1>
-        <NextLogo className="text-White70 w-1/4 inline-block" />
-        <VercelLogo className="text-White70 w-1/4 inline-block" />
+        <NextLogo className="text-White70 w-1/8 inline-block" />
+        <h2>test</h2>
+        <h3>test</h3>
+        <p>test</p>
+        <Link href="/about" className="text-off-white underline">About</Link>
+        <button className="bg-cta text-off-white px-4 py-2 rounded-md m-4 cursor-pointer">Button</button>
       </section>
     </>
   );
