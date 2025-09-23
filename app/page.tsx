bc5b19d7fc38d38798f120430a2946483f07685a
Olivier Bellemare <o.bellemare@soinsamika.com>
2025-09-23 14:27:08 -0400

    latest add text

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <h1>Home page</h1>
         <NextLogo className="text-White70 w-1/4 inline-block my-10" />
         <h2 className="mb-4"><span className="bg-White70 px-4 py-1 inline-block rounded-md">What is Lorem Ipsum?</span></h2>
-        <h3 className="mb-4"><span className="bg-White30 px-4 py-1 inline-block">Lorem Ipsum is simply dummy text</span></h3>
+        <h3 className="mb-4"><span className="bg-White30 px-4 py-1 inline-block">Lorem Ipsum is simply dummy text latest</span></h3>
         <p className="w-1/2 mx-auto">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
         <Link href="/about" className="text-off-white underline">Go to About page</Link>
         <button className="bg-cta text-off-white px-4 py-2 rounded-md m-4 cursor-pointer">Button</button>
